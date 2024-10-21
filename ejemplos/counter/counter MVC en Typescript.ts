3999ceff04295794d3df45730f2ca055a8d2e66c
jgverges <55912813+jgverges@users.noreply.github.com>
2024-10-21 10:53:25 +0200

    Create counter MVC en Typescript.ts

---
+++ ejemplos/counter/counter MVC en Typescript.ts
@@ -0,0 +1,46 @@
+// MODEL
+class Count {
+  constructor(private count: number = 0) {}
+  get() {
+    return this.count;
+  }
+  increment() {
+    this.count += 1;
+  }
+  decrement() {
+    if (this.count > 0) this.count -= 1;
+  }
+}
+
+// CONTROLLER
+class Controller {
+  constructor(private model: Count, private view: View) {}
+  get() {
+    this.view.render(this.model.get());
+  }
+  increment() {
+    this.model.increment();
+    this.view.render(this.model.get());
+  }
+  decrement() {
+    this.model.decrement();
+    this.view.render(this.model.get());
+  }
+}
+
+// View
+interface View {
+  render(val: number): void;
+}
+class LoggerView implements View {
+  render(val: number): void {
+    console.log(val);
+  }
+}
+
+const main = new Controller(new Count(), new LoggerView());
+console.log("ON");
+main.get();
+main.increment();
+main.increment();
+main.decrement();
